22e202b9821886853334db71341c9f6cf9d0d665
AmirNogerov <86724696+AmirNogerov@users.noreply.github.com>
2021-06-30 17:15:15 +0300

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter A and B: ";
+
+    double a, b;
+    int max, min;
+    cin >> a >> b;
+
+    max = a + b;
+    if (a - b > max)
+        max = a - b;
+
+    if (a * b > max)
+        max = a * b;
+
+    if (a / b > max)
+        max = a / b;
+
+    min = a + b;
+    if (a - b < min)
+        min = a - b;
+
+    if (a * b < min)
+        min = a * b;
+
+    if (a / b < min)
+        min = a / b;
+
+
+
+    cout << "A + B = " << a + b << '\n'
+        << "A - B = " << a - b << '\n'
+        << "A * B = " << a * b << '\n'
+        << "A / B = " << a / b << '\n'
+
+        << "max = " << max << '\n'
+
+        << "min = " << min << '\n';
+
+
+
+    return 0;
+}
